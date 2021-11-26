caeea8caf0b8ee4a2bc8cf35a107bedf3044f7db
abbas_mojarrad <abbasmojarrad7@gmail.com>
2021-11-26 16:09:48 +0330

    finish project v1

---
+++ src/script.js
@@ -0,0 +1,22 @@
+const $ = document
+const bgElm = $.querySelector('.background')
+const btnElm = $.querySelector('.btn')
+
+function generateColor () {
+  const color = `rgb(${Math.floor(Math.random() * 256)},${Math.floor(
+    Math.random() * 256
+  )},${Math.floor(Math.random() * 256)})`
+  return color
+}
+function getColor () {
+  const bgColor = `linear-gradient(-45deg,${generateColor()},${generateColor()},${generateColor()},${generateColor()})`
+  return bgColor
+}
+function addColor () {
+  const color = getColor()
+  console.log(color)
+  bgElm.style.backgroundImage = color
+}
+
+btnElm.addEventListener('click', addColor)
+$.onload = addColor()
